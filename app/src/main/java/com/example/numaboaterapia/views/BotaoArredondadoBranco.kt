c1a7a5fe125ad1b95ba17f7c2fa61f1d35382f1e
amanda.soares <amanda.soares@ifood.com.br>
2023-01-25 17:07:56 -0300

    adicionando layout dos botoes

---
+++ app/src/main/java/com/example/numaboaterapia/views/BotaoArredondadoBranco.kt
@@ -0,0 +1,16 @@
+package com.example.numaboaterapia.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.numaboaterapia.R
+
+class BotaoArredondadoBranco(
+    context: Context,
+    attrs: AttributeSet
+) : ConstraintLayout(context, attrs) {
+
+    private val view = View.inflate(context, R.layout.layout_white_button, this)
+
+}
