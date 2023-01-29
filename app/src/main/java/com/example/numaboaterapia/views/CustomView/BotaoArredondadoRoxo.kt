b9de1e8dfe1fdb9cff37738314d0dddb321b0aac
amanda.soares <amanda.soares@ifood.com.br>
2023-01-29 17:40:40 -0300

    Layout da tela de login pronto

---
+++ app/src/main/java/com/example/numaboaterapia/views/CustomView/BotaoArredondadoRoxo.kt
@@ -0,0 +1,17 @@
+package com.example.numaboaterapia.views.CustomView
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.numaboaterapia.R
+
+class BotaoArredondadoRoxo(
+    context: Context,
+    attrs: AttributeSet
+) : ConstraintLayout(context, attrs) {
+
+    private val view = View.inflate(context, R.layout.layout_purple_button, this)
+
+
+}
