393401adb5810e0365675598a6169987a0108a64
amanda.soares <amanda.soares@ifood.com.br>
2023-02-07 18:07:39 -0300

    Recuperar senha pronto

--- app/src/main/java/com/example/numaboaterapia/Login/view/PasswordRecoveryMessage.kt
+++ app/src/main/java/com/example/numaboaterapia/Login/view/PasswordRecoveryMessage.kt
@@ -1,12 +1,31 @@
 package com.example.numaboaterapia.Login.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.numaboaterapia.R
+import com.example.numaboaterapia.databinding.ActivityPasswordRecoveryMessageBinding
 
 class PasswordRecoveryMessage : AppCompatActivity() {
+    private lateinit var binding: ActivityPasswordRecoveryMessageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_password_recovery_message)
+
+        binding = ActivityPasswordRecoveryMessageBinding.inflate(layoutInflater)
+
+        setUp()
+
+        setContentView(binding.root)
+    }
+
+    private fun setUp(){
+        binding.buttonBackToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tryAgain.setOnClickListener {
+            finish()
+        }
     }
 }
