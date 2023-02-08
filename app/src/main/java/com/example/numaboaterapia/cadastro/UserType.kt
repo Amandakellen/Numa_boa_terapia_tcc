2220a696e540601f19179a83767743eb196ad050
amanda.soares <amanda.soares@ifood.com.br>
2023-02-08 15:17:04 -0300

    User Type Activity

---
+++ app/src/main/java/com/example/numaboaterapia/cadastro/UserType.kt
@@ -0,0 +1,12 @@
+package com.example.numaboaterapia.cadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.numaboaterapia.R
+
+class UserType : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_type)
+    }
+}
