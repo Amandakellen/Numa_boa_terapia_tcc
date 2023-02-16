dc4a6b0387e288ab4da44944b7ec7852abf8202b
amanda.soares <amanda.soares@ifood.com.br>
2023-02-16 20:25:01 -0300

    mvvm do cadastro

--- app/src/main/java/com/example/numaboaterapia/register/paciente/view/PatientRegistrationData.kt
+++ app/src/main/java/com/example/numaboaterapia/register/paciente/view/PatientRegistrationData.kt
@@ -2,12 +2,15 @@ package com.example.numaboaterapia.register.paciente.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import com.example.numaboaterapia.R
 import com.example.numaboaterapia.databinding.ActivityPatientRegistrationDataBinding
 
 class PatientRegistrationData : AppCompatActivity() {
 
     private lateinit var binding : ActivityPatientRegistrationDataBinding
+    private lateinit var toastRegistration: Toast
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +18,22 @@ class PatientRegistrationData : AppCompatActivity() {
         binding =  ActivityPatientRegistrationDataBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+    }
+
+    private fun setUpEditText() {
+//        binding.textInputEditEmail.doOnTextChanged { text, start, before, count ->
+//            viewModel.emailValue(text.toString())
+//        }
+//
+//        binding.textInputEditSenha.doOnTextChanged { text, start, before, count ->
+//            viewModel.senhaValue(text.toString())
+//        }
+    }
+
+
+    fun setUpToast(toastMessage: String) {
+        toastRegistration= Toast.makeText(
+            applicationContext, toastMessage, Toast.LENGTH_LONG
+        )
     }
 }
