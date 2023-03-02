2d78386e2d2cc270a439bf2032044534f22611c8
amanda.soares <amanda.soares@ifood.com.br>
2023-03-02 20:55:02 -0300

    repository dos motivos de contato

---
+++ app/src/main/java/com/example/numaboaterapia/register/pacient/data/repository/PatientConsultationReasonRepository.kt
@@ -0,0 +1,25 @@
+package com.example.numaboaterapia.register.pacient.data.repository
+
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class PatientConsultationReasonRepository {
+
+    val firebaseUserMutableLiveData: MutableLiveData<FirebaseUser>?
+    val userLoggedMutableLiveData: MutableLiveData<Boolean>
+    private val auth: FirebaseAuth
+    private val db = Firebase.firestore
+
+    init {
+        firebaseUserMutableLiveData = MutableLiveData()
+        userLoggedMutableLiveData = MutableLiveData()
+
+        auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            firebaseUserMutableLiveData.postValue(auth.currentUser)
+        }
+    }
+}
