409de7fb1d6f892ff95add47295adf7245dd5e05
amanda.soares <amanda.soares@ifood.com.br>
2023-03-05 17:47:16 -0300

    Melhorando o layout do toolbar

--- app/src/main/java/com/example/numaboaterapia/register/pacient/view/ActivityPatientConsultationReason.kt
+++ app/src/main/java/com/example/numaboaterapia/register/pacient/view/ActivityPatientConsultationReason.kt
@@ -2,6 +2,7 @@ package com.example.numaboaterapia.register.pacient.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.numaboaterapia.R
 import com.example.numaboaterapia.databinding.ActivityPatientConsultationReasonBinding
 import com.example.numaboaterapia.register.pacient.view.adapters.ConsultationReasonAdapter
 import com.example.numaboaterapia.register.pacient.viewModel.PatientConsultationReasonViewModel
@@ -14,18 +15,26 @@ class ActivityPatientConsultationReason : AppCompatActivity() {
 
         binding = ActivityPatientConsultationReasonBinding.inflate(layoutInflater)
         val recyclerView = binding.reasonRecyclerView
-//        viewModel = PatientConsultationReasonViewModel()
-//
-//        val adapter = ConsultationReasonAdapter()
-//
-//        recyclerView.adapter = adapter
-//
-//        adapter.data = viewModel.getDataItens()
+        viewModel = PatientConsultationReasonViewModel()
+
+        val adapter = ConsultationReasonAdapter()
+
+        recyclerView.adapter = adapter
+
+        adapter.data = viewModel.getDataItens()
 
         setContentView(binding.root)
 
     }
 
+    private fun setUpViews(){
+        binding.toolbarView.getViewById(R.id.back_button).setOnClickListener {
+            finish()
+        }
+        binding.toolbarView
+
+
+    }
 
 
 }
--- app/src/main/java/com/example/numaboaterapia/views/CustomView/PatientInformationToolBar.kt
+++ app/src/main/java/com/example/numaboaterapia/views/CustomView/PatientInformationToolBar.kt
@@ -3,24 +3,25 @@ package com.example.numaboaterapia.views.CustomView
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.numaboaterapia.R
 
-class PatientInformationToolBarcontext(
+class PatientInformationToolBar(
     context: Context,
     attrs: AttributeSet
-) : ConstraintLayout(context, attrs) {
+) : FrameLayout(context, attrs) {
 
     private val view = View.inflate(context, R.layout.tool_bar_patient_information, this)
     private val backButton = view.findViewById<ImageButton>(R.id.patient_back_button)
-    private val title = view.findViewById<TextView>(R.id.tiltle)
-    private val step = view.findViewById<TextView>(R.id.step_number)
+    val title = view.findViewById<TextView>(R.id.tiltle)
+    val step = view.findViewById<TextView>(R.id.step_number)
 
     fun getBackButton(): ImageButton = backButton
 
-    fun setTitleText(text : String) = text.also { this.title.text = it }
-
-    fun setStepText(text : String) = text.also { this.step.text = it }
+//    fun setTitleText(text : Int) = title.setText(text)
+//
+//    fun setStepText(text : Int) = step.setText(text)
 }
