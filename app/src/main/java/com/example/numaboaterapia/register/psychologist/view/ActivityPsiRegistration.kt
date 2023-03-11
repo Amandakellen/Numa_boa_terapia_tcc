1d56f7393f8112e88073ba6e9646e01dd83935cc
amanda.soares <amanda.soares@ifood.com.br>
2023-03-10 21:01:44 -0300

    layout da tela de cadastro do psi

---
+++ app/src/main/java/com/example/numaboaterapia/register/psychologist/view/ActivityPsiRegistration.kt
@@ -0,0 +1,16 @@
+package com.example.numaboaterapia.register.psychologist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.numaboaterapia.R
+import com.example.numaboaterapia.databinding.ActivityPsiRegistrationBinding
+
+class ActivityPsiRegistration : AppCompatActivity() {
+    private lateinit var binding : ActivityPsiRegistrationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPsiRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
