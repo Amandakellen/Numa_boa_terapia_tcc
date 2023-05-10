be2bec891553baca39e1b6b8f2832844de1f6d1f
amanda.soares <amanda.soares@ifood.com.br>
2023-05-10 20:46:09 -0300

    Abrindo a camera

--- app/src/main/java/com/example/numaboaterapia/appNavigation/pacient/views/bottomsheet/SelectPhotoBottomSheet.kt
+++ app/src/main/java/com/example/numaboaterapia/appNavigation/pacient/views/bottomsheet/SelectPhotoBottomSheet.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.example.numaboaterapia.R
 import com.example.numaboaterapia.databinding.SelectPhotoBottomSheetBinding
@@ -19,6 +20,8 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 class SelectPhotoBottomSheet : BottomSheetDialogFragment() {
 
     private lateinit var binding: SelectPhotoBottomSheetBinding
+    private val REQUEST_CAMERA_PERMISSION = 1
+    private val REQUEST_IMAGE_CAPTURE = 2
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,35 +38,63 @@ class SelectPhotoBottomSheet : BottomSheetDialogFragment() {
     private fun setUpButtons() {
         binding.selectPhotoFromCamera.setText(R.string.take_picture)
 
-        binding.selectPhotoFromCamera.setOnClickListener {
-            if (ActivityCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.CAMERA
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                activity?.let {
-                    ActivityCompat.requestPermissions(
-                        it,
-                        arrayOf(Manifest.permission.CAMERA),
-                        0
-                    )
-                }
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            activity?.let {
+                ActivityCompat.requestPermissions(
+                    it,
+                    arrayOf(Manifest.permission.CAMERA),
+                    0
+                )
             }
-            tirarPhoto()
+        }
+        binding.selectPhotoFromCamera.setOnClickListener {
+
+            takePicture()
         }
 
     }
 
-    private fun tirarPhoto() {
+    private fun takePicture() {
 
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        // Verifique se a permissão da câmera foi concedida pelo usuário
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Crie uma Intent para abrir a câmera
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
-        val someActivityResultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    // Handle the result.
-                }
+            // Verifique se existe um aplicativo de câmera disponível
+            if (takePictureIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
+                // Inicie a atividade da câmera com startActivityForResult()
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
             }
-        someActivityResultLauncher.launch(intent)
+        } else {
+            // Solicite permissão de câmera ao usuário
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CAMERA_PERMISSION
+            )
+        }
+
     }
+
+//    private fun tirarPhoto() {
+//
+//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//
+//        val someActivityResultLauncher =
+//            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//                if (result.resultCode == Activity.RESULT_OK) {
+//                    // Handle the result.
+//                }
+//            }
+//        someActivityResultLauncher.launch(intent)
+//    }
 }
---
+++ app/src/main/java/com/example/numaboaterapia/camera/views/PreviewPhotoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.numaboaterapia.camera.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.numaboaterapia.R
+
+class PreviewPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview_photo)
+    }
+}
