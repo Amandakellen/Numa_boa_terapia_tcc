dc0749d3168231fb5308de584e09668b644ebe52
amanda.soares <amanda.soares@ifood.com.br>
2023-05-23 20:11:47 -0300

    User type sem abrir a home direto

--- app/src/main/java/com/example/numaboaterapia/register/userType/view/UserType.kt
+++ app/src/main/java/com/example/numaboaterapia/register/userType/view/UserType.kt
@@ -3,11 +3,11 @@ package com.example.numaboaterapia.register.userType.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.numaboaterapia.appNavigation.pacient.views.PacientApp
+import com.example.numaboaterapia.appNavigation.psychologist.views.PsiHome
 import com.example.numaboaterapia.databinding.ActivityUserTypeBinding
 import com.example.numaboaterapia.register.pacient.view.PatientRegistrationData
 import com.example.numaboaterapia.register.psychologist.view.ActivityPsiRegistration
-import com.example.numaboaterapia.register.psychologist.view.PsiBiography
-import com.example.numaboaterapia.register.psychologist.view.SelectSignature
 
 
 class UserType : AppCompatActivity() {
@@ -29,13 +29,13 @@ class UserType : AppCompatActivity() {
         }
 
         binding.clientImage.setOnClickListener {
-            val intent = Intent(this, PatientRegistrationData::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, PatientRegistrationData::class.java))
+
         }
 
         binding.psiImage.setOnClickListener {
-           //startActivity(Intent(this, ActivityPsiRegistration::class.java))
-            startActivity(Intent(this, PsiBiography::class.java))
+            startActivity(Intent(this, ActivityPsiRegistration::class.java))
+
         }
 
 
