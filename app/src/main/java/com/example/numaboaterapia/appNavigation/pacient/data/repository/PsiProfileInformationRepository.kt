678570c10d2d2e76067d46d9fa8b834f3da678d1
amanda <amandakellensoares@hotmail.com>
2023-05-24 17:56:40 -0300

    colocando o retorno do registro do psi no repository do psiPofileInformation

--- app/src/main/java/com/example/numaboaterapia/appNavigation/pacient/data/repository/PsiProfileInformationRepository.kt
+++ app/src/main/java/com/example/numaboaterapia/appNavigation/pacient/data/repository/PsiProfileInformationRepository.kt
@@ -1,9 +1,16 @@
 package com.example.numaboaterapia.appNavigation.pacient.data.repository
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.tasks.await
 
 class PsiProfileInformationRepository {
     private val firebaseUserMutableLiveData: MutableLiveData<FirebaseUser>?
@@ -22,4 +29,27 @@ class PsiProfileInformationRepository {
             firebaseUserMutableLiveData.postValue(auth.currentUser)
         }
     }
+
+    fun getRegiterCollection(userUUID : String): Flow<ArrayList<String>> = flow {
+        val docRef = db.collection("psi_users")
+        val query = docRef.whereEqualTo("uId", userUUID)
+        val registerList = ArrayList<String>()
+        val documents = query.get().await()
+        for (document in documents) {
+
+            registerList.add(document.data.getValue("psi_name").toString())
+            registerList.add(document.data.getValue("psi_email").toString())
+            registerList.add(document.data.getValue("psi_phone").toString())
+            registerList.add(document.data.getValue("psi_linkwpp").toString())
+            registerList.add(document.data.getValue("psi_time").toString())
+            registerList.add(document.data.getValue("psi_crp").toString())
+            registerList.add(document.data.getValue("_psi_especializacao").toString())
+
+
+        }
+        emit(registerList)
+    }.catch { exception ->
+        Log.e("getCollection", "Error getting collection", exception)
+        emit(ArrayList<String>())
+    }.flowOn(Dispatchers.IO)
 }
