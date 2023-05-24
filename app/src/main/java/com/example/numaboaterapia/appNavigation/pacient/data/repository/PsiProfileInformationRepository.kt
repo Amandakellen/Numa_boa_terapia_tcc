8c11a917037ce4947df6728191bd3d95dfd52444
amanda <amandakellensoares@hotmail.com>
2023-05-24 17:52:23 -0300

    comecando o repository do profile information

--- app/src/main/java/com/example/numaboaterapia/appNavigation/pacient/data/repository/PsiProfileInformationRepository.kt
+++ app/src/main/java/com/example/numaboaterapia/appNavigation/pacient/data/repository/PsiProfileInformationRepository.kt
@@ -1,4 +1,25 @@
 package com.example.numaboaterapia.appNavigation.pacient.data.repository
 
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+
 class PsiProfileInformationRepository {
+    private val firebaseUserMutableLiveData: MutableLiveData<FirebaseUser>?
+    private val userLoggedMutableLiveData: MutableLiveData<Boolean>
+    private val auth: FirebaseAuth
+    private var psiUid : ArrayList<String> = arrayListOf()
+    private lateinit var imageDefault : ByteArray
+    val db = FirebaseFirestore.getInstance()
+
+    init {
+        firebaseUserMutableLiveData = MutableLiveData()
+        userLoggedMutableLiveData = MutableLiveData()
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null) {
+            firebaseUserMutableLiveData.postValue(auth.currentUser)
+        }
+    }
 }
