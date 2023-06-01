8daa7022d0eb3bf76ed6805d2c35fe5aea18bd35
amanda.soares <amanda.soares@ifood.com.br>
2023-05-31 21:03:06 -0300

    Ordenando os dados do historico do diario

--- app/src/main/java/com/example/numaboaterapia/appNavigation/pacient/data/repository/PatientDiaryRepository.kt
+++ app/src/main/java/com/example/numaboaterapia/appNavigation/pacient/data/repository/PatientDiaryRepository.kt
@@ -6,6 +6,7 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -15,7 +16,7 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.tasks.await
 
 
-class PatientDiaryRepository(val colectionName :String) {
+class PatientDiaryRepository(val colectionName: String) {
 
     val firebaseUserMutableLiveData: MutableLiveData<FirebaseUser>?
     val userLoggedMutableLiveData: MutableLiveData<Boolean>
@@ -38,29 +39,31 @@ class PatientDiaryRepository(val colectionName :String) {
     }
 
 
+    fun getCollection(): Flow<HashMap<Int, Any>> = flow {
+        val userUUID = auth.currentUser!!.uid
+        val docRef = db.collection("patient_diary")
+        val query =
+            docRef.whereEqualTo("uId", userUUID).
+            orderBy("diary_date", Query.Direction.DESCENDING)
+        val dataHashMap = HashMap<Int, Any>()
+        val documents = query.get().await()
+        var i = 0
+        for (document in documents) {
+            val hasMapFirebase = hashMapOf(
+                "diary_feeling" to document.data.getValue("diary_feeling"),
+                "diary_text" to document.data.getValue("diary_text"),
+                "diary_date" to document.data.getValue("diary_date")
+            )
+            textList.add(document.data.getValue("diary_text").toString())
+            dateList.add(document.data.getValue("diary_date").toString())
+            feelingList.add(document.data.getValue("diary_feeling").toString())
+            dataHashMap[i] = hasMapFirebase
+            i++
+        }
+        emit(dataHashMap)
+    }.catch { exception ->
+        Log.e("getCollection", "Error getting collection", exception)
+        emit(HashMap<Int, Any>())
+    }.flowOn(Dispatchers.IO)
 
-     fun getCollection(): Flow<HashMap<Int, Any>>  = flow {
-         val userUUID = auth.currentUser!!.uid
-         val docRef = db.collection("patient_diary")
-         val query = docRef.whereEqualTo("uId", userUUID)
-         val dataHashMap = HashMap<Int, Any>()
-         val documents = query.get().await()
-         var i = 0
-         for (document in documents) {
-             val hasMapFirebase = hashMapOf(
-                 "diary_feeling" to document.data.getValue("diary_feeling"),
-                 "diary_text" to document.data.getValue("diary_text"),
-                 "diary_date" to document.data.getValue("diary_date")
-             )
-             textList.add(document.data.getValue("diary_text").toString())
-             dateList.add(document.data.getValue("diary_date").toString())
-             feelingList.add(document.data.getValue("diary_feeling").toString())
-             dataHashMap.put(i, hasMapFirebase)
-             i++
-         }
-         emit(dataHashMap)
-     }.catch { exception ->
-         Log.e("getCollection", "Error getting collection", exception)
-         emit(HashMap<Int, Any>())
-     }.flowOn(Dispatchers.IO)
 }
