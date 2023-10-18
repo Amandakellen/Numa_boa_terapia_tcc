292d032c399ea867642e53b84a54c07e5a5e9a07
Amanda Soares <amanda.soares@ifood.com.br>
2023-10-18 19:23:10 -0300

    Comecando com as configurações do compose

---
+++ app/src/main/java/com/example/numaboaterapia/ui/theme/Color.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.numaboaterapia.ui.theme
+
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF794A99)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFF4DAFF)
+val md_theme_light_onPrimaryContainer = Color(0xFF2F004C)
+val md_theme_light_secondary = Color(0xFF6135EE)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFE6DEFF)
+val md_theme_light_onSecondaryContainer = Color(0xFF1C0062)
+val md_theme_light_tertiary = Color(0xFF984061)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFD9E2)
+val md_theme_light_onTertiaryContainer = Color(0xFF3E001D)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFF8FDFF)
+val md_theme_light_onBackground = Color(0xFF001F25)
+val md_theme_light_surface = Color(0xFFF8FDFF)
+val md_theme_light_onSurface = Color(0xFF001F25)
+val md_theme_light_surfaceVariant = Color(0xFFEADFEA)
+val md_theme_light_onSurfaceVariant = Color(0xFF4B454D)
+val md_theme_light_outline = Color(0xFF7C747E)
+val md_theme_light_inverseOnSurface = Color(0xFFD6F6FF)
+val md_theme_light_inverseSurface = Color(0xFF00363F)
+val md_theme_light_inversePrimary = Color(0xFFE3B5FF)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF794A99)
+val md_theme_light_outlineVariant = Color(0xFFCDC3CE)
+val md_theme_light_scrim = Color(0xFF000000)
+
+val md_theme_dark_primary = Color(0xFFE3B5FF)
+val md_theme_dark_onPrimary = Color(0xFF481867)
+val md_theme_dark_primaryContainer = Color(0xFF60317F)
+val md_theme_dark_onPrimaryContainer = Color(0xFFF4DAFF)
+val md_theme_dark_secondary = Color(0xFFCABEFF)
+val md_theme_dark_onSecondary = Color(0xFF32009A)
+val md_theme_dark_secondaryContainer = Color(0xFF4800D6)
+val md_theme_dark_onSecondaryContainer = Color(0xFFE6DEFF)
+val md_theme_dark_tertiary = Color(0xFFFFB1C8)
+val md_theme_dark_onTertiary = Color(0xFF5E1133)
+val md_theme_dark_tertiaryContainer = Color(0xFF7B2949)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFD9E2)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF001F25)
+val md_theme_dark_onBackground = Color(0xFFA6EEFF)
+val md_theme_dark_surface = Color(0xFF001F25)
+val md_theme_dark_onSurface = Color(0xFFA6EEFF)
+val md_theme_dark_surfaceVariant = Color(0xFF4B454D)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCDC3CE)
+val md_theme_dark_outline = Color(0xFF978E98)
+val md_theme_dark_inverseOnSurface = Color(0xFF001F25)
+val md_theme_dark_inverseSurface = Color(0xFFA6EEFF)
+val md_theme_dark_inversePrimary = Color(0xFF794A99)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFE3B5FF)
+val md_theme_dark_outlineVariant = Color(0xFF4B454D)
+val md_theme_dark_scrim = Color(0xFF000000)
+
+
+val seed = Color(0xFFE2C4F1)
